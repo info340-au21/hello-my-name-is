e1d64016faef60849ec83b030010e268d125037b
yufeiz6 <yufeiz6@uw.edu>
2021-11-21 14:36:41 -0800

    table

---
+++ src/components/Bookmark/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export function header() {
+    return (
+        <header>
+            <div className="book-container">
+                <h1>Bookmarked names</h1>
+                <p className="fav name">Here is the list of your favorite names</p>
+            </div>
+        </header>
+    )
+}
