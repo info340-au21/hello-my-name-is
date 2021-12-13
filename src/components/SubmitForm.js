ef2d82bb40118259e99541268e782505489786be
J. Maynard-Zhang <maynjas19@gmail.com>
2021-12-13 14:08:26 -0800

    Improve UX of SubmitForm page

--- src/components/SubmitForm.js
+++ src/components/SubmitForm.js
@@ -124,31 +124,33 @@ export function SubmitForm(props) {
     } else {
         return (
             <main>
+                <h2 className="center">Submit a name to the database!</h2>
+
                 <form role='form' method='GET'>
                     <div className="form-attribute">
-                        <label htmlFor="name">What name would you like to submit to the data base? </label>
+                        <label htmlFor="name">What <strong>name</strong> would you like to submit? </label>
                         <input id="name" onBlur={handleName} type = "text" placeholder="Enter a name" className="search" />
                     </div>
     
                     <div className="form-attribute">
-                        <label htmlFor="pronunciation">How do you pronounce this name?</label>
+                        <label htmlFor="pronunciation">How do you <strong>pronounce</strong> this name?</label>
                         <input id="pronunciation" onBlur={handlePronunciation} type="text" placeholder="eg. nah-loo" className="search"></input>
                     </div>
     
                     <div className="form-attribute">
-                        <label htmlFor="origin">What is the origin of this name? </label>
+                        <label htmlFor="origin">What is the <strong>origin</strong> of this name? </label>
                         <input id="origin" onBlur={handleOrigin} type = "text" placeholder="eg. Greek, Somolian" className="search" />
                     </div>
     
                     <div className="form-attribute">
-                        <label htmlFor="gender">Is it a gendered name? </label>
-                        <label htmlFor="feminine"><input id="gender" value="feminine" onInput={handleGender} type = "checkbox" /> Feminine </label>
-                        <label htmlFor="masculine"><input id="gender" value="masculine" onInput={handleGender} type = "checkbox" /> Masculine </label>
-                        <label htmlFor="nuetral"><input id="gender" value="nuetral" onInput={handleGender} type = "checkbox" /> Neutral </label>
+                        <label htmlFor="gender">Is it a <strong>gendered</strong> name? </label>
+                        <label htmlFor="feminine"><input id="feminine" value="feminine" onInput={handleGender} type = "checkbox" /> Feminine </label>
+                        <label htmlFor="masculine"><input id="masculine" value="masculine" onInput={handleGender} type = "checkbox" /> Masculine </label>
+                        <label htmlFor="neutral"><input id="neutral" value="nuetral" onInput={handleGender} type = "checkbox" /> Neutral </label>
                     </div>
     
                     <div className="form-attribute">
-                        <label htmlFor="meaning">What is the meaning of this name? </label>
+                        <label htmlFor="meaning">What is the <strong>meaning</strong> of this name? </label>
                         <input id="meaning" onBlur={handleMeaning} type = "text" placeholder="max 180 characters" />
                     </div>
     
